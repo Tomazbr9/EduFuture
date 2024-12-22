26ca66cb3370ab0b182adac44b5a3c983c5da987
Bruno Tomaz <tomazcraft@gmail.com>
2024-12-22 08:48:21 -0300

    Criei os modelos course, module e class

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Course, Module, Classe
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price']
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+
+@admin.register(Classe)
+class ClasseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'module']
+
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+class Base(models.Model):
+    creation = models.DateTimeField(auto_now_add=True) # Data de criação
+    update = models.DateTimeField(auto_now=True) # Data de atualização
+
+    class Meta:
+        abstract = True 
+
+class Course(Base):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.0'))
+    image = models.ImageField(upload_to='courses', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+class Module(Base):
+    title = models.CharField(max_length=255)
+    course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Modulo'
+        verbose_name_plural = 'Modulos'
+
+class Classe(models.Model):
+    title = models.CharField(max_length=255)
+    module = models.ForeignKey(Module, related_name='classes' ,on_delete=models.CASCADE)
+    materials = models.FileField(upload_to='materials', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Aula'
+        verbose_name_plural = 'Aulas'
